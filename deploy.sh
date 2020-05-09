82b17f1bddd6675fdfd51d40b8a581c9f98d20c1
dileep1484 <mittadileep@gmail.com>
2020-05-09 14:32:00 +0800

    Added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t dileep1484/multi-client:latest -t dileep1484/multi-client:$SHA -f .client/Dockerfile ./client
+docker build -t dileep1484/multi-server:latest -t dileep1484/multi-server:$SHA -f .server/Dockerfile ./server
+docker build -t dileep1484/multi-worker:latest -t dileep1484/multi-worker:$SHA -f .worker/Dockerfile ./worker
+
+docker push dileep1484/multi-client:latest
+docker push dileep1484/multi-server:latest
+docker push dileep1484/multi-worker:latest
+docker push dileep1484/multi-client:$SHA
+docker push dileep1484/multi-server:$SHA
+docker push dileep1484/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=dileep1484/multi-server:$SHA
+kubectl set image deployments/client-deployment client=dileep1484/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=dileep1484/multi-worker:$SHA
